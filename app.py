7dd1ac5283ef056d97654a0b18f3fa50011ddad9
aliaa <alia.tarik@stu.eng.aswu.edu.eg>
2025-06-10 18:10:03 +0300

    Initial commit of Flask app with frontend

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from roboflow import Roboflow
 import base64
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
